9c68940359ce1958cd9d7b239d22796d10d21ef7
freddietheodo <freddiew@theodo.co.uk>
2023-06-08 17:28:33 +0100

    fix: fixed-typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: { popup: "./src/popup.jsx", background: "./src/background.jsx" },
+  entry: { popup: "./src/popup.tsx", background: "./src/background.tsx" },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: { 
-          loader: "babel-loader" ,
+        use: {
+          loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ["@babel/preset-react"],
           },
         },
       },
@@ -24,12 +34,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/popup.html',
-    filename: 'popup.html'
-  }),
+      template: "./src/popup.html",
+      filename: "popup.html",
+    }),
     new CopyPlugin({
-      patterns: [
-        { from: 'public'}
-      ]
-    })]
+      patterns: [{ from: "public" }],
+    }),
+  ],
 };
