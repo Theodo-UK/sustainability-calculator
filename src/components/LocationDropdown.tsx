d9ef66cfc3c209798074c175f1aae1d7d2589bdd
Justin <57725347+juskek@users.noreply.github.com>
2023-06-13 11:41:41 +0100

    Show select location in dropdown button

--- src/components/LocationDropdown.tsx
+++ src/components/LocationDropdown.tsx
@@ -11,14 +11,15 @@ export type LocationType = {
 
 
 export const LocationDropdown = () => {
-  const [selectedLocation, setSelectedLocation] = useState<LocationType>(LOCATIONS[0])
+  const [selectedLocation, setSelectedLocation] = useState<LocationType>()
 
   return (
     <div className="fixed top-16 w-72">
       <Listbox value={selectedLocation} onChange={setSelectedLocation}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selectedLocation.country}</span>
+            
+            <span className="block truncate">{selectedLocation ? selectedLocation.country : 'Select a location'}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               {/* <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
