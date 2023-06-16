ad4ccb7b09c2e43dc10f8ae912021c7bd08eaa0b
Justin <57725347+juskek@users.noreply.github.com>
2023-06-16 12:55:58 +0100

    Replace dropdown icon with plus icon

--- src/components/CountryDropdown.tsx
+++ src/components/CountryDropdown.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Fragment } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
-import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { PlusIcon } from '@heroicons/react/20/solid'
 import { Countries, CountryName } from '../constants/Countries';
 
 
@@ -19,7 +19,7 @@ export const CountryDropdown = ({ addToSelectedCountries }: CountryDropdownType)
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
             <span className="block truncate">Add a country</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon
+              <PlusIcon
                 className="h-5 w-5 text-gray-400"
                 aria-hidden="true"
               />
