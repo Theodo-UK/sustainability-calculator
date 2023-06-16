e3cc773c5fb8908f74e6a5aa9c46188eae4ba0b3
Justin <57725347+juskek@users.noreply.github.com>
2023-06-16 14:33:48 +0100

    Added tests for addToSelectedCountries and removeSelectedCountry

--- src/__tests__/usePopup.tests.ts
+++ src/__tests__/usePopup.tests.ts
@@ -23,13 +23,13 @@ describe('usePopup', () => {
         jest.clearAllMocks();
     });
 
-    
+
     test('addToSelectedCountries should update selectedCountries', () => {
         const { result } = renderHook(() => usePopup());
-        
+
         const mockCountry1 = "Australia";
         const mockCountry2 = "United Kingdom";
-        
+
         const mockCountries: Set<CountryName> = new Set();
 
         mockCountries.add(mockCountry1);
@@ -37,11 +37,42 @@ describe('usePopup', () => {
             result.current.addToSelectedCountries(mockCountry1);
         });
         expect(result.current.selectedCountries).toStrictEqual(mockCountries);
-        
+
         mockCountries.add(mockCountry2);
         act(() => {
             result.current.addToSelectedCountries(mockCountry2);
         });
+        expect(result.current.selectedCountries).toStrictEqual(mockCountries);
+    });
+
+    test('removeSelectedCountries should update selectedCountries', () => {
+        const { result } = renderHook(() => usePopup());
+
+        const mockCountry1 = "Australia";
+        const mockCountry2 = "United Kingdom";
+
+        const mockCountries: Set<CountryName> = new Set();
+       
+
+        mockCountries.add(mockCountry1);
+        mockCountries.add(mockCountry2);
+
+        act(() => {
+            result.current.addToSelectedCountries(mockCountry1);
+        });
+        
+        act(() => {
+            result.current.addToSelectedCountries(mockCountry2);
+        });
+
+        expect(result.current.selectedCountries).toStrictEqual(mockCountries);
+        
+        mockCountries.delete(mockCountry2);
+
+        act(() => {
+            result.current.removeSelectedCountry(mockCountry2);
+        });
+        
         expect(result.current.selectedCountries).toStrictEqual(mockCountries);
     });
 
