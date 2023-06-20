b923ed8743926cb3432501d4ed2c96494ec0cf50
Justin <57725347+juskek@users.noreply.github.com>
2023-06-20 15:14:12 +0100

    Remove unused import

--- src/__tests__/usePopup.tests.ts
+++ src/__tests__/usePopup.tests.ts
@@ -2,7 +2,6 @@ import { renderHook, act } from '@testing-library/react';
 
 import { COUNTRIES, CountryName, WORLDAVERAGE } from '../constants/Countries';
 import { areMapsDeepEqual } from '../helpers/areMapsDeepEqual';
-import { mock } from 'node:test';
 import { usePopup } from '../components/usePopup';
 
 
