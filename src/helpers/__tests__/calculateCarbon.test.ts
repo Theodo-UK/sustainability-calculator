4b334b63def78701db007cfa2f7ca9387469e516
Justin <57725347+juskek@users.noreply.github.com>
2023-06-20 17:18:59 +0100

    Updated formula for calculateCarbon

--- src/helpers/__tests__/calculateCarbon.test.ts
+++ src/helpers/__tests__/calculateCarbon.test.ts
@@ -1,12 +1,33 @@
 import { CountryName } from "../../constants/Countries"
 import { calculateCarbon } from "../calculateCarbon"
 
-const mockCountries: Map<CountryName, number> = new Map([
-    ["Australia", 0]
+const mockCountry: Map<CountryName, number> = new Map([
+    ["Australia", 0.5]
 ]);
+
+const mockCountries: Map<CountryName, number> = new Map([
+    ["Australia", 0.2],
+    ["United Kingdom", 0.3],
+    ["Belgium", 0.2],
+    ["Bulgaria", 0.2],
+    ["Croatia", 0.1],
+]);
+// "United Kingdom" : 212.3,
+//     "Austria" : 111.2,
+//     "Belgium" : 161.9,
+//     "Bulgaria" : 372.1,
+//     "Croatia" : 227,
+
+
+const mockSmallTransferSize = 50 
+const mockLargeTransferSize = 500000 
 
 describe('calculateCarbon', () => {
     it('country should return corresponding value', () => {
-        expect(calculateCarbon(mockCountries)).toEqual(0)
+        expect(calculateCarbon(mockSmallTransferSize, mockCountry)).toBeCloseTo(0.000017695)
+    })
+
+    it('multiple countries should return corresponding carbon consumption for a given transfer size', () => {
+        expect(calculateCarbon(mockLargeTransferSize, mockCountries)).toBeCloseTo(0.16074)
     })
 })
--- src/helpers/calculateCarbon.ts
+++ src/helpers/calculateCarbon.ts
@@ -1,5 +1,10 @@
-import { CountryName } from "../constants/Countries";
+import { COUNTRIES, CountryName } from "../constants/Countries";
 
-export const calculateCarbon = (countries: Map<CountryName,number>): number => {
-    return 0;
+export const calculateCarbon = (transferSize: number, selectedCountries: Map<CountryName, number>): number => {
+
+    let carbon = 0;
+    selectedCountries.forEach((percentage, country) => {
+        carbon += (transferSize / 1073741824) * COUNTRIES[country] * percentage;
+    });
+    return carbon;
 }
