c7077c838885d3761800621587aac568096e320b
Justin <57725347+juskek@users.noreply.github.com>
2023-06-28 12:18:55 +0100

    Create IEmissionsRepository interface

---
+++ src/data/emissions/IEmissionsRepository.ts
@@ -0,0 +1,18 @@
+import { EmissionsRepository } from "./EmissionsRepository";
+
+export abstract class IEmissionsRepository {
+    private static _instance: IEmissionsRepository;
+    static get instance(): IEmissionsRepository {
+        this._instance = new EmissionsRepository();
+        return this._instance;
+    }
+
+    abstract storeLastCalculation(emissionsData: EmissionsData): Promise<void>;
+
+    abstract getLastCalculation(): Promise<EmissionsData>;
+}
+
+export type EmissionsData = {
+    bytes: number;
+    emissions: number;
+};
