229718d921b471afc9c19039ba551961d5dda935
Justin <57725347+juskek@users.noreply.github.com>
2023-06-28 11:09:23 +0100

    Format test files with ESLint

--- src/components/__tests__/LocationDropdown.test.tsx
+++ src/components/__tests__/LocationDropdown.test.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { CountryDropdown } from "components/CountryDropdown";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
-
-describe('CountryDropdown', () => {
-    it('Dropdown button should show add a country when no countries are selected', () => {
+describe("CountryDropdown", () => {
+    it("Dropdown button should show add a country when no countries are selected", () => {
         const addSelectedCountry = jest.fn();
         const { getByText } = render(
             <CountryDropdown addSelectedCountry={addSelectedCountry} />
         );
 
-        expect(getByText('Add a country')).toBeInTheDocument();
+        expect(getByText("Add a country")).toBeInTheDocument();
     });
-}
-)
+});
--- src/helpers/__tests__/areMapsDeepEqual.test.ts
+++ src/helpers/__tests__/areMapsDeepEqual.test.ts
@@ -1,40 +1,32 @@
-import { CountryName } from "../../constants/Countries"
+import { CountryName } from "../../constants/Countries";
 import { areMapsDeepEqual } from "../areMapsDeepEqual";
 
 const mockMap1: Map<CountryName, number> = new Map([
-    ['United Kingdom', 0],
-    ['Australia', 10],
+    ["United Kingdom", 0],
+    ["Australia", 10],
 ]);
 const mockMap2: Map<CountryName, number> = new Map([
-    ['United Kingdom', 0],
-    ['Australia', 10],
+    ["United Kingdom", 0],
+    ["Australia", 10],
 ]);
-const mockMap3: Map<CountryName, number> = new Map([
-    ['United Kingdom', 0],
-]);
-const mockMap4: Map<CountryName, number> = new Map([
-    ['United Kingdom', 10],
-]);
-const mockMap5: Map<CountryName, number> = new Map([
-    ['Australia', 10],
-]);
+const mockMap3: Map<CountryName, number> = new Map([["United Kingdom", 0]]);
+const mockMap4: Map<CountryName, number> = new Map([["United Kingdom", 10]]);
+const mockMap5: Map<CountryName, number> = new Map([["Australia", 10]]);
 
-
-describe('areMapsDeepEqual', () => {
-    it('maps should be equal', () => {
+describe("areMapsDeepEqual", () => {
+    it("maps should be equal", () => {
         expect(areMapsDeepEqual(mockMap1, mockMap2)).toBe(true);
     });
 
-    it('maps should not be equal when of different length', () => {
+    it("maps should not be equal when of different length", () => {
         expect(areMapsDeepEqual(mockMap1, mockMap3)).toBe(false);
     });
 
-    it('maps should not be equal when there is a different value', () => {
+    it("maps should not be equal when there is a different value", () => {
         expect(areMapsDeepEqual(mockMap3, mockMap4)).toBe(false);
     });
 
-    it('maps should not be equal when there is a different key', () => {
+    it("maps should not be equal when there is a different key", () => {
         expect(areMapsDeepEqual(mockMap4, mockMap5)).toBe(false);
     });
-
 });
