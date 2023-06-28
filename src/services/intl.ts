a7d10cb0894f99910f719b00549758885886c794
Justin <57725347+juskek@users.noreply.github.com>
2023-06-28 11:30:42 +0100

    Formatting internationalisation files with ESLint

--- src/services/intl.ts
+++ src/services/intl.ts
@@ -1,24 +1,24 @@
 type Message = string | NestedDictionary;
 type NestedDictionary = {
-  [x: string]: Message;
+    [x: string]: Message;
 };
 type FlattenedDictionary = {
-  [x: string]: string;
+    [x: string]: string;
 };
 
 export const flattenMessages = (
-  nestedMessages: NestedDictionary,
-  prefix = ""
+    nestedMessages: NestedDictionary,
+    prefix = ""
 ): FlattenedDictionary =>
-  Object.keys(nestedMessages).reduce((messages: FlattenedDictionary, key) => {
-    const value = nestedMessages[key];
-    const prefixedKey = prefix !== "" ? `${prefix}.${key}` : key;
+    Object.keys(nestedMessages).reduce((messages: FlattenedDictionary, key) => {
+        const value = nestedMessages[key];
+        const prefixedKey = prefix !== "" ? `${prefix}.${key}` : key;
 
-    if (typeof value === "string") {
-      messages[prefixedKey] = value;
-    } else {
-      Object.assign(messages, flattenMessages(value, prefixedKey));
-    }
+        if (typeof value === "string") {
+            messages[prefixedKey] = value;
+        } else {
+            Object.assign(messages, flattenMessages(value, prefixedKey));
+        }
 
-    return messages;
-  }, {});
+        return messages;
+    }, {});
