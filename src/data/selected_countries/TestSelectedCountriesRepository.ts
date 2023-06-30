092aea435e0d04f22ca60ba965a21dd5ead46aa1
Justin <57725347+juskek@users.noreply.github.com>
2023-06-30 15:26:42 +0100

    Added TestRepositories for SelectedCountries and Emissions for mocking

---
+++ src/data/selected_countries/TestSelectedCountriesRepository.ts
@@ -0,0 +1,45 @@
+import { CountryName } from "../../constants/Countries";
+import { ISelectedCountriesRepository } from "./ISelectedCountriesRepository";
+import { SelectedCountriesRemoteDataSource } from "./SelectedCountriesRemoteDataSource";
+
+export class TestSelectedCountriesRepository
+    implements ISelectedCountriesRepository
+{
+    private _selectedCountries: Map<CountryName, number> = new Map<
+        CountryName,
+        number
+    >([["World Average", 0]]);
+
+    async getSelectedCountriesAndPercentages(): Promise<
+        Map<CountryName, number>
+    > {
+        return this._selectedCountries;
+    }
+
+    async addSelectedCountry(countryName: CountryName): Promise<void> {
+        const newMap = new Map(this._selectedCountries);
+        if (!newMap.has(countryName)) {
+            newMap.set(countryName, 0);
+        }
+        this._selectedCountries = newMap;
+    }
+
+    async removeSelectedCountry(countryName: CountryName): Promise<void> {
+        const newMap = new Map(this._selectedCountries);
+        if (newMap.has(countryName)) {
+            newMap.delete(countryName);
+        }
+        this._selectedCountries = newMap;
+    }
+
+    async setSelectedCountryPercentage(
+        countryName: CountryName,
+        percentage: number
+    ): Promise<void> {
+        const newMap = new Map(this._selectedCountries);
+        if (newMap.has(countryName)) {
+            newMap.set(countryName, percentage);
+        }
+        this._selectedCountries = newMap;
+    }
+}
