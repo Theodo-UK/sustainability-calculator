a9f3f4bae3168ba9e4af916768772f1358d71290
Justin <57725347+juskek@users.noreply.github.com>
2023-07-04 17:03:02 +0100

    Ignoring explicit any in StorageRemoteDataSource since typing matches API; typing of args and and return types done in Repository layer of app

--- src/data/storage/StorageRemoteDataSource.ts
+++ src/data/storage/StorageRemoteDataSource.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export class StorageRemoteDataSource {
     private storage = (() => {
         let mutex: Promise<void> | null = Promise.resolve();
