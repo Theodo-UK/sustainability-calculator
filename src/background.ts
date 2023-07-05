567b495e957c59517c2c7f8e7fc824cbf6547586
Justin <57725347+juskek@users.noreply.github.com>
2023-07-05 17:44:20 +0100

    Added comments

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@ import { IBytesRepository } from "./data/bytes/IBytesRepository";
 const webRequestOnCompleteListener = (
     details: chrome.webRequest.WebResponseCacheDetails
 ) => {
+    // Catch all response header+body size
     const headerSize =
         details.responseHeaders?.reduce((acc, header) => {
             const encoder = new TextEncoder();
@@ -27,6 +28,7 @@ const webRequestOnCompleteListener = (
 const webRequestOnBeforeRequestListener = (
     details: chrome.webRequest.WebRequestBodyDetails
 ) => {
+    // Catch POST request body size
     const bodySize = details.requestBody?.raw?.[0].bytes?.byteLength;
     if (bodySize) {
         IBytesRepository.instance.addBytesTransferred(bodySize);
@@ -36,6 +38,7 @@ const webRequestOnBeforeRequestListener = (
 const webRequestOnBeforeSendHeaders = (
     details: chrome.webRequest.WebRequestHeadersDetails
 ) => {
+    // Catch all requests header size
     const headerSize = details.requestHeaders?.reduce((acc, header) => {
         const encoder = new TextEncoder();
         const headerLength = encoder.encode(
