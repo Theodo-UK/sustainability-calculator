570df58bf1fa32c264a808a01b0e73efbb2385fa
François Lorne <francois.lorne@hotmail.com>
2023-07-17 17:39:28 +0100

    fix: remove timestamp conversion to second before string formating

--- src/utils/helpers/unixTimeToDateString.ts
+++ src/utils/helpers/unixTimeToDateString.ts
@@ -1,4 +1,4 @@
 export function unixTimeToDateString(unixTimeMs: number) {
-    const date = new Date(unixTimeMs * 1000);
+    const date = new Date(unixTimeMs);
     return date.toLocaleString("en-GB", { timeZone: "UTC" });
 }
