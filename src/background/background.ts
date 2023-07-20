a1da0aac31f95621b8fd49ca1f56d25d894633f7
Justin <57725347+juskek@users.noreply.github.com>
2023-07-20 17:49:49 +0100

    revert: use loadingFinished instead, removed unnecessary functions

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,5 @@
 import { IBytesRepository } from "../data/bytes/IBytesRepository";
-import { NetworkRequestManager } from "./network_request_manager/NetworkRequestManager";
-import { formatBytesString } from "../view/popup/utils/formatBytesString";
+import { addBytesTransferred } from "./backgroundHelpers";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === "getBytesTransferred") {
@@ -37,107 +36,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 chrome.debugger.onEvent.addListener(function (source, method, params) {
     switch (method) {
-        case "Network.responseReceived": {
-            const { requestId, response } = params as {
-                requestId?: string;
-                response?: {
-                    encodedDataLength?: number;
-                };
-            };
-            if (
-                requestId === undefined ||
-                response === undefined ||
-                response.encodedDataLength === undefined
-            ) {
-                throw new Error(
-                    `requestId or encodedDataLength is undefined: \nrequestId: ${requestId} \nencodedDataLength: ${response?.encodedDataLength}`
-                );
-            }
-            NetworkRequestManager.setRequestTransferSize(
-                requestId,
-                response.encodedDataLength
-            );
-
-            break;
-        }
-        case "Network.dataReceived": {
-            const { requestId, encodedDataLength } = params as {
-                requestId?: string;
-                encodedDataLength?: number;
-            };
-            if (requestId === undefined || encodedDataLength === undefined) {
-                throw new Error(
-                    `requestId or encodedDataLength is undefined: \nrequestId: ${requestId} \nencodedDataLength: ${encodedDataLength}`
-                );
-            }
-            NetworkRequestManager.increaseRequestTransferSize(
-                requestId,
-                encodedDataLength
-            );
-
-            break;
-        }
         case "Network.loadingFinished": {
-            const { requestId, encodedDataLength } = params as {
-                requestId?: string;
+            const { encodedDataLength } = params as {
                 encodedDataLength?: number;
             };
-            if (requestId === undefined || encodedDataLength === undefined) {
-                throw new Error(
-                    `requestId or encodedDataLength is undefined: \nrequestId: ${requestId} \nencodedDataLength: ${encodedDataLength}`
-                );
+            if (encodedDataLength) {
+                addBytesTransferred(encodedDataLength);
             }
-            NetworkRequestManager.setRequestTransferSize(
-                requestId,
-                encodedDataLength
-            );
-
-            break;
-        }
-        case "Network.loadingFailed": {
-            const { requestId } = params as {
-                requestId?: string;
-            };
-            if (requestId === undefined) {
-                throw new Error("requestId is undefined");
-            }
-            NetworkRequestManager.setRequestTransferSize(requestId, -1);
-
-            break;
-        }
-
-        case "Network.webSocketClosed": {
-            const { requestId } = params as {
-                requestId?: string;
-            };
-            if (requestId === undefined) {
-                throw new Error("requestId is undefined");
-            }
-            NetworkRequestManager.setRequestTransferSize(requestId, -1);
-
-            break;
-        }
-        case "Network.webTransportClosed": {
-            const { requestId } = params as {
-                requestId?: string;
-            };
-            if (requestId === undefined) {
-                throw new Error("requestId is undefined");
-            }
-            NetworkRequestManager.setRequestTransferSize(requestId, -1);
-
-            break;
-        }
-        case "Network.requestWillBeSent": {
-            const { requestId } = params as {
-                requestId?: string;
-            };
-            if (requestId === undefined) {
-                throw new Error("requestId is undefined");
-            }
-
-            NetworkRequestManager.addRequest(requestId);
-
             break;
         }
         default:
---
+++ src/background/backgroundHelpers.ts
@@ -0,0 +1,27 @@
+import { IBytesRepository } from "../data/bytes/IBytesRepository";
+
+export const addBytesTransferred = async (bytes: number) => {
+    IBytesRepository.instance.addBytesTransferred(bytes);
+
+    try {
+        await chrome.runtime.sendMessage({
+            command: {
+                bytesTransferredChanged:
+                    IBytesRepository.instance.getBytesTransferred(),
+            },
+        });
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            if (
+                e.message ===
+                "Could not establish connection. Receiving end does not exist."
+            ) {
+                console.log(
+                    `Error Caught: ${e}\nIf popup is open and this error is seen in the console, debugging is required.`
+                );
+            }
+        } else {
+            throw e;
+        }
+    }
+};
