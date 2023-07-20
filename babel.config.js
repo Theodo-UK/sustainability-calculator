f87ab5103e81747f691d58c8be518b804e3c26ce
rsamuel12 <rohansamm@gmail.com>
2023-07-20 17:46:41 +0100

    chore: fix all possible linting issues

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
-  api.cache(true);
+    api.cache(true);
 
-  const presets = ['@babel/preset-env',
-    "@babel/preset-react", "@babel/preset-typescript"];
+    const presets = [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+    ];
 
-  return {
-    presets,
-  };
+    return {
+        presets,
+    };
 };
