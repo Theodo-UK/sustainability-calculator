e1c4c13904fa2902a19d5c7f41842b30c461b5ba
François Lorne <francois.lorne@hotmail.com>
2023-07-20 15:45:41 +0100

    style: rearrange the recording page

--- src/view/popup/pages/RecordingPage.tsx
+++ src/view/popup/pages/RecordingPage.tsx
@@ -1,42 +1,46 @@
 import React from "react";
-import { Button } from "../../components/atomic/Button";
 import {
     formatBytes,
     formatEmissions,
 } from "../../../utils/helpers/formatNumbersToString";
-import { getEmissionsComparison } from "../../../utils/helpers/getEmissionComparison";
+import { ITooltip } from "../../../view/components/atomic/ITooltip";
+import { CircularButton } from "../../components/atomic/CircularButton";
 
 type RecordingPageProps = {
     onStopButtonPress: () => void;
     bytesTransferred: number;
-    averageSpecificEmissions: number;
     emissions: number;
 };
 
 export const RecordingPage = ({
     onStopButtonPress,
     bytesTransferred,
-    averageSpecificEmissions,
     emissions,
 }: RecordingPageProps) => {
     return (
-        <div className="p-10 w-80">
-            <h1 className="text-3xl font-bold underline">
-                Sustainability Calculator
-            </h1>
-            <p>Total Data Received: {formatBytes(bytesTransferred)}</p>
-            <p>
-                Specific Carbon Emissions:
-                {` ${formatEmissions(averageSpecificEmissions)} per gigabyte`}
-            </p>
-            <p>Software Carbon Intensity: {formatEmissions(emissions)}</p>
-            <p>
-                CO2 emissions are equivalent to:{" "}
-                {getEmissionsComparison(emissions)}
-            </p>
-            <Button onClick={onStopButtonPress} colour="red">
-                Stop recording
-            </Button>
-        </div>
+        <>
+            <div className="absolute top-1 right-1">
+                <ITooltip
+                    text="Stopping this recording will store<br />the data in your calculations history"
+                    id="recording-page"
+                />
+            </div>
+            <div className="p-10 pb-20 w-96 flex flex-col justify-stretch gap-6">
+                <h1 className="text-2xl font-bold text-center">
+                    Recording in progress
+                </h1>
+                <div className="py-2 px-4 gap-2 text-base bg-nyanza text-center rounded-2xl shadow font-medium">
+                    <p>{formatBytes(bytesTransferred)}</p>
+                    <p>{formatEmissions(emissions)}</p>
+                </div>
+                <CircularButton
+                    text="Stop Recording"
+                    onClick={onStopButtonPress}
+                    colour="burgundy"
+                >
+                    -
+                </CircularButton>
+            </div>
+        </>
     );
 };
