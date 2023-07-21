0edb4cfba045d41d464570e2f90a5f680a4b4c57
François Lorne <francois.lorne@hotmail.com>
2023-07-21 15:10:47 +0100

    style: pimp the design of the calculation history

--- src/view/components/calculation-history/CalculationHistory.tsx
+++ src/view/components/calculation-history/CalculationHistory.tsx
@@ -5,6 +5,7 @@ import {
     formatEmissions,
     unixTimeToDateString,
 } from "../../../utils/helpers/formatNumbersToString";
+import { Button } from "../atomic/Button";
 
 type CountryDropdownType = {
     calculationHistory: CalculationDataType[];
@@ -13,36 +14,65 @@ type CountryDropdownType = {
 export const CalculationHistory = ({
     calculationHistory,
 }: CountryDropdownType) => {
+    const [isListClosed, setIsListClosed] = React.useState(true);
     return (
-        <div>
-            <ul>
-                {calculationHistory.map((calculation, index) => (
-                    <li key={index}>
-                        <p>Bytes: {formatBytes(calculation.bytes)}</p>
-                        <p>
-                            Emissions: {formatEmissions(calculation.emissions)}
-                        </p>
-                        <p>
-                            Specific Emissions:{" "}
-                            {formatEmissions(calculation.specificEmissions)}
-                        </p>
-                        <p>
-                            Date & Time:{" "}
-                            {unixTimeToDateString(calculation.unixTimeMs)}
-                        </p>
-                        <p>User: {calculation.userType}</p>
-                        <ul>
-                            {Array.from(calculation.selectedCountries).map(
-                                ([countryName, percentage]) => (
-                                    <li key={countryName}>
-                                        {countryName}: {percentage}
-                                    </li>
-                                )
-                            )}
-                        </ul>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            {isListClosed ? (
+                <Button
+                    colour="light-green"
+                    type="text"
+                    onClick={() => setIsListClosed(false)}
+                >
+                    View History
+                </Button>
+            ) : (
+                <ul className="overflow-y-scroll h-96 rounded-2xl shadow bg-nyanza p-4">
+                    <h2 className="font-semibold text-center mb-2 text-lg">
+                        Calculation History
+                    </h2>
+                    {calculationHistory.map((calculation, index) => {
+                        const [day, time] = unixTimeToDateString(
+                            calculation.unixTimeMs
+                        ).split(", ");
+                        return (
+                            <li
+                                key={index}
+                                className="p-2 rounded-2xl hover:shadow-inner transition-shadow duration-200"
+                            >
+                                <h3 className="font-semibold">
+                                    {`Recording n°${
+                                        calculationHistory.length - index
+                                    }:`}
+                                </h3>
+                                <p className="ml-4 mb-2">
+                                    {`On the ${day} at ${time} UTC`}
+                                    <br />
+                                    {`As a ${calculation.userType}`}
+                                    <br />
+                                    {`Bytes: ${formatBytes(calculation.bytes)}`}
+                                    <br />
+                                    {`Emissions: ${formatEmissions(
+                                        calculation.emissions
+                                    )} g of CO2`}
+                                    <br />
+                                    {`Specific Emissions: ${formatEmissions(
+                                        calculation.specificEmissions
+                                    )} g of CO2`}
+                                </p>
+                                <ul>
+                                    {Array.from(
+                                        calculation.selectedCountries
+                                    ).map(([countryName, percentage]) => (
+                                        <li key={countryName}>
+                                            {countryName}: {percentage}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </>
     );
 };
