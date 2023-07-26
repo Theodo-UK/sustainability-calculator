ea32cd50b94c8f262131518a04be57d930762a60
Justin <57725347+juskek@users.noreply.github.com>
2023-07-26 12:52:45 +0100

    fix:do not go to RecordingPage if chrome URL

--- src/view/popup/utils/refreshActiveTabAndRecordBytes.ts
+++ src/view/popup/utils/refreshActiveTabAndRecordBytes.ts
@@ -1,21 +1,22 @@
-export const refreshActiveTabAndRecordBytes = async (bypassCache: boolean) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length > 0) {
-            const tabId = tabs[0].id;
-            if (tabId) {
-                chrome.tabs.reload(
-                    tabId,
-                    {
-                        bypassCache: bypassCache,
-                    },
-                    () => {
-                        chrome.runtime.sendMessage({
-                            command: "startRecordingBytesTransferred",
-                            tabId,
-                        });
-                    }
-                );
+export const refreshActiveTabAndRecordBytes = async (
+    bypassCache: boolean
+): Promise<void> => {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tabs.length > 0) {
+        const tabId = tabs[0].id;
+        if (tabId) {
+            await chrome.tabs.reload(tabId, {
+                bypassCache: bypassCache,
+            });
+            const { success, message } = await chrome.runtime.sendMessage({
+                command: "startRecordingBytesTransferred",
+                tabId,
+            });
+
+            if (!success) {
+                throw new Error(message);
             }
         }
-    });
+    }
 };
