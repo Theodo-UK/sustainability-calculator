c1849df059ec0bde6468b1dbc7f83757acaef59a
Justin <57725347+juskek@users.noreply.github.com>
2023-07-26 09:19:41 +0100

    refactor:created CalculationData class to replace CalculationDataType

--- src/data/calculations/ICalculationsRepository.ts
+++ src/data/calculations/ICalculationsRepository.ts
@@ -1,3 +1,4 @@
+import { JSONtoMap, maptoJSON } from "../../utils/helpers/jsonHelpers";
 import { CountryName } from "../constants/CountryEmissions";
 import { CalculationsRepository } from "./CalculationsRepository";
 import { TestCalculationsRepository } from "./TestCalculationsRepository";
@@ -44,3 +45,40 @@ export type CalculationDataType = {
     unixTimeMs: number;
     userType: UserType;
 };
+
+export class CalculationData {
+    constructor(
+        public bytes: number,
+        public emissions: number,
+        public specificEmissions: number,
+        public selectedCountries: Map<CountryName, number>,
+        public unixTimeMs: number,
+        public userType: UserType
+    ) {}
+
+    public toJSON(): string {
+        return JSON.stringify({
+            bytes: this.bytes,
+            emissions: this.emissions,
+            specificEmissions: this.specificEmissions,
+            selectedCountries: maptoJSON(this.selectedCountries),
+            unixTimeMs: this.unixTimeMs,
+            userType: this.userType,
+        });
+    }
+
+    public static fromJSON(json: string): CalculationData {
+        const obj = JSON.parse(json);
+        const selectedCountries = JSONtoMap<CountryName, number>(
+            obj.selectedCountries
+        );
+        return new CalculationData(
+            obj.bytes,
+            obj.emissions,
+            obj.specificEmissions,
+            selectedCountries,
+            obj.unixTimeMs,
+            obj.userType
+        );
+    }
+}
