2eea0b06b6018340af63add1018fc77872ffb9f6
Justin <57725347+juskek@users.noreply.github.com>
2023-07-26 09:20:41 +0100

    refactor:added methods to convert CalculationData[] to and from JSON

--- src/utils/helpers/jsonHelpers.ts
+++ src/utils/helpers/jsonHelpers.ts
@@ -1,3 +1,5 @@
+import { CalculationData } from "../../data/calculations/ICalculationsRepository";
+
 export const maptoJSON = <K, V>(map: Map<K, V>): string => {
     return JSON.stringify(Object.fromEntries(map));
 };
@@ -5,3 +7,17 @@ export const maptoJSON = <K, V>(map: Map<K, V>): string => {
 export const JSONtoMap = <K, V>(jsonStr: string): Map<K, V> => {
     return new Map<K, V>(Object.entries(JSON.parse(jsonStr)) as Array<[K, V]>);
 };
+
+export function calculationDataArrayToJSON(
+    calculationDataArray: CalculationData[]
+): string {
+    return JSON.stringify(
+        calculationDataArray.map((calculationData) => calculationData.toJSON())
+    );
+}
+
+export function JSONtoCalculationDataArray(json: string): CalculationData[] {
+    return JSON.parse(json).map((jsonStr: string) =>
+        CalculationData.fromJSON(jsonStr)
+    );
+}
