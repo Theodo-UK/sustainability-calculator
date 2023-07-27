b1fd53543775bbc001255835036fa136a04848a0
Justin <57725347+juskek@users.noreply.github.com>
2023-07-27 09:56:18 +0100

    chore: created background helpers for readability

--- src/background/backgroundHelpers.ts
+++ src/background/backgroundHelpers.ts
@@ -23,3 +23,70 @@ export const addBytesTransferred = async (bytes: number) => {
         }
     }
 };
+
+export type StartRecordingBytesTransferredReturnType = {
+    success: boolean;
+    message: string;
+};
+
+export const startRecordingBytesTransferred = async (
+    tabId: number,
+    sendResponse: (response: StartRecordingBytesTransferredReturnType) => void
+): Promise<void> => {
+    IBytesRepository.instance.clearBytesTransferred();
+
+    chrome.tabs.get(tabId).then((tab) => {
+        if (tab.url && tab.url.startsWith("chrome://")) {
+            sendResponse({
+                success: false,
+                message:
+                    "Cannot calculate emissions for a chrome:// URL, e.g. manage extensions page. Please try again on a valid webpage.",
+            });
+            return;
+        } else {
+            chrome.debugger.attach({ tabId }, "1.2").then(() => {
+                chrome.debugger.sendCommand(
+                    { tabId },
+                    "Network.enable",
+                    {},
+                    () => {
+                        if (chrome.runtime.lastError) {
+                            console.error(chrome.runtime.lastError);
+                        }
+                        sendResponse({
+                            success: true,
+                            message:
+                                "Successfully started recording bytes transferred.",
+                        });
+                        return;
+                    }
+                );
+            });
+        }
+    });
+};
+
+export const stopRecordingBytesTransferred = async (
+    tabId: number,
+    sendResponse: (response: boolean) => void
+): Promise<void> => {
+    chrome.debugger
+        .detach({ tabId })
+        .then(() => {
+            sendResponse(true);
+            return;
+        })
+        .catch((e: unknown) => {
+            if (
+                (e as Error).message ===
+                `Debugger is not attached to the tab with id: ${tabId}.`
+            ) {
+                console.warn(
+                    `Tried to detach debugger from tab (tabId: ${tabId}) when there was none attached. `
+                );
+                sendResponse(false);
+                return;
+            }
+            throw e;
+        });
+};
