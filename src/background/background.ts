5b74463ec05f82c6e69afcf0d7220cce35c618db
Justin <57725347+juskek@users.noreply.github.com>
2023-07-27 09:56:18 +0100

    chore: created background helpers for readability

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,9 @@
 import { IBytesRepository } from "../data/bytes/IBytesRepository";
-import { addBytesTransferred } from "./backgroundHelpers";
+import {
+    addBytesTransferred,
+    startRecordingBytesTransferred,
+    stopRecordingBytesTransferred,
+} from "./backgroundHelpers";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === "getBytesTransferred") {
@@ -9,55 +13,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { tabId } = message;
 
     if (message.command === "startRecordingBytesTransferred") {
-        IBytesRepository.instance.clearBytesTransferred();
-
-        chrome.tabs.get(tabId).then((tab) => {
-            if (tab.url && tab.url.startsWith("chrome://")) {
-                sendResponse({
-                    success: false,
-                    message:
-                        "Cannot calculate emissions for a chrome:// URL, e.g. manage extensions page. Please try again on a valid webpage.",
-                });
-            } else {
-                chrome.debugger.attach({ tabId: tabId }, "1.2").then(() => {
-                    chrome.debugger.sendCommand(
-                        { tabId: tabId },
-                        "Network.enable",
-                        {},
-                        () => {
-                            if (chrome.runtime.lastError) {
-                                console.error(chrome.runtime.lastError);
-                            }
-                            sendResponse({
-                                success: true,
-                                message:
-                                    "Successfully started recording bytes transferred.",
-                            });
-                        }
-                    );
-                });
-            }
-        });
+        startRecordingBytesTransferred(tabId, sendResponse);
     }
 
     if (message.command === "stopRecordingBytesTransferred") {
-        chrome.debugger
-            .detach({ tabId: tabId })
-            .then(() => {
-                sendResponse(true);
-            })
-            .catch((e: unknown) => {
-                if (
-                    (e as Error).message ===
-                    `Debugger is not attached to the tab with id: ${tabId}.`
-                ) {
-                    console.warn(
-                        `Tried to detach debugger from tab (tabId: ${tabId}) when there was none attached. `
-                    );
-                    return;
-                }
-                throw e;
-            });
+        stopRecordingBytesTransferred(tabId, sendResponse);
     }
     return true;
 });
--- src/background/backgroundHelpers.ts
+++ src/background/backgroundHelpers.ts
@@ -10,16 +10,83 @@ export const addBytesTransferred = async (bytes: number) => {
                     IBytesRepository.instance.getBytesTransferred(),
             },
         });
-    } catch (e: unknown) {
+    } catch (error: unknown) {
         if (
-            (e as Error).message ===
+            (error as Error).message ===
             "Could not establish connection. Receiving end does not exist."
         ) {
             console.warn(
-                `Error Caught: ${e}\nIf popup is open and this error is seen in the console, debugging is required.`
+                `Error Caught: ${error}\nIf popup is open and this error is seen in the console, debugging is required.`
             );
         } else {
-            throw e;
+            throw error;
         }
     }
+};
+
+export type StartRecordingBytesTransferredReturnType = {
+    success: boolean;
+    message: string;
+};
+
+export const startRecordingBytesTransferred = async (
+    tabId: number,
+    sendResponse: (response: StartRecordingBytesTransferredReturnType) => void
+): Promise<void> => {
+    IBytesRepository.instance.clearBytesTransferred();
+
+    chrome.tabs.get(tabId).then((tab) => {
+        if (tab.url && tab.url.startsWith("chrome://")) {
+            sendResponse({
+                success: false,
+                message:
+                    "Cannot calculate emissions for a chrome:// URL, e.g. manage extensions page. Please try again on a valid webpage.",
+            });
+            return;
+        } else {
+            chrome.debugger.attach({ tabId }, "1.2").then(() => {
+                chrome.debugger.sendCommand(
+                    { tabId },
+                    "Network.enable",
+                    {},
+                    () => {
+                        if (chrome.runtime.lastError) {
+                            console.error(chrome.runtime.lastError);
+                        }
+                        sendResponse({
+                            success: true,
+                            message:
+                                "Successfully started recording bytes transferred.",
+                        });
+                        return;
+                    }
+                );
+            });
+        }
+    });
+};
+
+export const stopRecordingBytesTransferred = async (
+    tabId: number,
+    sendResponse: (response: boolean) => void
+): Promise<void> => {
+    chrome.debugger
+        .detach({ tabId })
+        .then(() => {
+            sendResponse(true);
+            return;
+        })
+        .catch((error: unknown) => {
+            if (
+                (error as Error).message ===
+                `Debugger is not attached to the tab with id: ${tabId}.`
+            ) {
+                console.warn(
+                    `Tried to detach debugger from tab (tabId: ${tabId}) when there was none attached. `
+                );
+                sendResponse(false);
+                return;
+            }
+            throw error;
+        });
 };
