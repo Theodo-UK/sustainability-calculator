13d1a94b467cc2bbf6d51d97f26283eed3e21b8f
Justin <57725347+juskek@users.noreply.github.com>
2023-07-27 16:45:48 +0100

    feat: create SelectedDevices, moved percentage error message to constants

--- src/view/components/selected-devices/selected/SelectedDevices.tsx
+++ src/view/components/selected-devices/selected/SelectedDevices.tsx
@@ -1,38 +1,16 @@
-import React, { useEffect } from "react";
-import { CountryName } from "../../../data/constants/CountryEmissions";
-import { PERCENTAGE_ERROR_MESSAGE } from "../../../utils/constants";
-import { TileTooltip } from "../atomic/TileTooltip";
+import React from "react";
+import { PERCENTAGE_ERROR_MESSAGE } from "../../../../utils/constants";
+import { TileTooltip } from "../../atomic/TileTooltip";
+import { useSelectedDevices } from "./useSelectedDevices";
 
-type SelectedCountriesType = {
-    selectedCountries: Map<CountryName, number>;
-    removeSelectedCountry: (country: CountryName) => void;
-    setCountryPercentage: (country: CountryName, percentage: number) => void;
-};
-
-export const SelectedCountries = ({
-    selectedCountries,
-    removeSelectedCountry,
-    setCountryPercentage,
-}: SelectedCountriesType) => {
-    const [worldPercentage, setWorldPercentage] = React.useState(1);
-    const [isPercentageError, setIsPercentageError] = React.useState(false);
-
-    useEffect(() => {
-        const totalPercentage = Array.from(selectedCountries.values()).reduce(
-            (total, entry) => total + entry,
-            0
-        );
-        setWorldPercentage(1 - totalPercentage);
-    }, [selectedCountries]);
-
-    useEffect(() => {
-        const hasPercentageError = Array.from(selectedCountries.values()).some(
-            (percentage) => percentage > 1 || percentage < 0
-        );
-        const worldHasError = worldPercentage > 1 || worldPercentage < 0;
-        setIsPercentageError(hasPercentageError || worldHasError);
-    }, [selectedCountries, worldPercentage]);
-
+export const SelectedDevices = () => {
+    const {
+        isPercentageError,
+        averagePercentage,
+        selectedDevices,
+        removeSelectedDevice,
+        setDevicePercentage,
+    } = useSelectedDevices();
     return (
         <div className="w-full relative">
             <div className="relative flex items-center text-sm w-full gap-1 p-2 my-2 bg-rose-quartz bg-opacity-20 rounded-lg">
@@ -40,19 +18,19 @@ export const SelectedCountries = ({
                     <input
                         className="w-12 text-gray-800 bg-gray-100 border border-gray-200 rounded-md shadow-sm pl-1 mr-1"
                         type="string"
-                        value={(worldPercentage * 100).toFixed(2)}
+                        value={(averagePercentage * 100).toFixed(2)}
                         disabled
                     />
                     <a>% of users following world average</a>
                 </div>
                 <TileTooltip
-                    text="The figure is used by default<br />where countries have not been<br />specified for a % of users"
+                    text="The figure is used by default<br />where devices have not been<br />specified for a % of users"
                     id="world-percentage"
                 />
             </div>
             <ul>
-                {Array.from(selectedCountries).map(([country, percentage]) => (
-                    <li key={country}>
+                {Array.from(selectedDevices).map(([device, percentage]) => (
+                    <li key={device}>
                         <div className="relative flex items-center text-sm w-full p-2 my-2 bg-rose-quartz bg-opacity-20 rounded-lg">
                             <div className="flex items-start max-w-[14rem]">
                                 <input
@@ -60,17 +38,17 @@ export const SelectedCountries = ({
                                     type="number"
                                     defaultValue={percentage * 100}
                                     onChange={(e) =>
-                                        setCountryPercentage(
-                                            country,
+                                        setDevicePercentage(
+                                            device,
                                             Number(e.target.value) / 100
                                         )
                                     }
                                 />
-                                <a>% in {country}</a>
+                                <a>% in {device}</a>
                             </div>
                             <button
                                 className="country-tile-button "
-                                onClick={() => removeSelectedCountry(country)}
+                                onClick={() => removeSelectedDevice(device)}
                             >
                                 -
                             </button>
