59de02a2eb529146d523e3567484fb66fa3f6735
Justin <57725347+juskek@users.noreply.github.com>
2023-07-27 10:13:33 +0100

    chore: rename tabId for readability

--- src/view/popup/utils/backgroundStopRecordingBytes.ts
+++ src/view/popup/utils/backgroundStopRecordingBytes.ts
@@ -1,11 +1,11 @@
 export const backgroundStopRecordingBytes = async () => {
     const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
     if (tabs.length > 0) {
-        const tabId = tabs[0].id;
-        if (tabId) {
+        const activeTabId = tabs[0].id;
+        if (activeTabId) {
             chrome.runtime.sendMessage({
                 command: "stopRecordingBytesTransferred",
-                tabId,
+                activeTabId,
             });
         }
     }
