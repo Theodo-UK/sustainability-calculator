0d9994fdff12203de711390f054f092c082a0615
Justin <57725347+juskek@users.noreply.github.com>
2023-08-01 10:35:20 +0100

    fix: move test for EmissionsComparison and correct path to images

--- src/view/components/results/comparison/EmissionsComparison.tsx
+++ src/view/components/results/comparison/EmissionsComparison.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { CalculationData } from "../../../../data/calculations/ICalculationsRepository";
 import { getEmissionsComparison } from "../../../../utils/helpers/getEmissionComparison";
 
+import CoffeeCupPath from "../../../../assets/coffee-cup.svg";
+import GoogleSearchPath from "../../../../assets/google-search.svg";
+import PlasticBottlePath from "../../../../assets/plastic-bottle.svg";
 import { RealLifeExample } from "../../../../data/constants/RealLifeComparison";
-import CoffeeCupPath from "../../../assets/coffee-cup.svg";
-import GoogleSearchPath from "../../../assets/google-search.svg";
-import PlasticBottlePath from "../../../assets/plastic-bottle.svg";
 
 export const EmissionsComparison = ({
     calculation,
