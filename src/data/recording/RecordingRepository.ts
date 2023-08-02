742a758a809c6c48a67b0a790c0560f805718cc1
Justin <57725347+juskek@users.noreply.github.com>
2023-08-02 10:50:13 +0100

    feat: create RecordingRepository.setStartUnixTimeMs and RecordingRepository.getStartUnixTimeMs

--- src/data/recording/RecordingRepository.ts
+++ src/data/recording/RecordingRepository.ts
@@ -14,4 +14,24 @@ export abstract class RecordingRepository {
             ongoingCalculation: ongoing,
         });
     }
+
+    static async setStartUnixTime(unixTimeMs: number | null): Promise<void> {
+        await IStorageRepository.instance.set({
+            ongoingCalculationStartUnixTimeMs: unixTimeMs,
+        });
+    }
+
+    static async getStartUnixTime(): Promise<number> {
+        const data = await IStorageRepository.instance.get<number | null>(
+            "ongoingCalculationStartUnixTimeMs",
+            null
+        );
+        if (data === null) {
+            throw Error(
+                "Tried to get start time when it was null. Start time is null when there is no ongoing calculation."
+            );
+        }
+
+        return data;
+    }
 }
