97bbf37ee3b3fed745144fafbed99acf987c95c6
Justin <57725347+juskek@users.noreply.github.com>
2023-08-02 11:41:54 +0100

    feat: show flow time in EmissionsSummary

--- src/utils/helpers/stringHelpers.ts
+++ src/utils/helpers/stringHelpers.ts
@@ -1,2 +1,11 @@
 export const removeWhiteSpace = (str: string): string =>
     str.replace(/\s+/g, "");
+
+export const secondsToTimeString = (seconds: number): string => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const paddedMinutes = minutes.toString().padStart(2, "0");
+    const paddedSeconds = remainingSeconds.toFixed(0).padStart(2, "0");
+    return `${paddedMinutes}:${paddedSeconds}`;
+};
--- src/view/components/results/summary/EmissionsSummary.tsx
+++ src/view/components/results/summary/EmissionsSummary.tsx
@@ -3,6 +3,7 @@ import {
     formatBytes,
     formatEmissions,
 } from "../../../../utils/helpers/formatNumbersToString";
+import { secondsToTimeString } from "../../../../utils/helpers/stringHelpers";
 import {
     HistoryContext,
     HistoryContextType,
@@ -12,17 +13,23 @@ import { useNullSafeContext } from "../../../provider/useNullSafeContext";
 export const EmissionsSummary = () => {
     const { calculationHistory } =
         useNullSafeContext<HistoryContextType>(HistoryContext);
-    if (!calculationHistory) {
-        return <div>Loading...</div>;
-    }
+
     return (
-        <p className="text-center flex flex-wrap content-center justify-center">
-            {formatBytes(calculationHistory[0].bytes)}
-            <br />
-            {`${formatEmissions(calculationHistory[0].specificEmissions)}
+        calculationHistory && (
+            <p className="text-center flex flex-wrap content-center justify-center">
+                {formatBytes(calculationHistory[0].bytes)}
+                <br />
+                {`${formatEmissions(calculationHistory[0].specificEmissions)}
         gCO2/GB`}
-            <br />
-            {`${formatEmissions(calculationHistory[0].emissions)} g of CO2`}
-        </p>
+                <br />
+                {`${formatEmissions(calculationHistory[0].emissions)} g of CO2`}
+                <br />
+                {`Flow Time: ${secondsToTimeString(
+                    (calculationHistory[0].endUnixTimeMs -
+                        calculationHistory[0].startUnixTimeMs) /
+                        1000
+                )}`}
+            </p>
+        )
     );
 };
