665e83f72fbe14f319127fd7d36d87c56ae0ee08
Justin <57725347+juskek@users.noreply.github.com>
2023-08-03 10:30:28 +0100

    fix: correct conversion from bytes to gigabytes

--- src/utils/helpers/calculateEmissions.ts
+++ src/utils/helpers/calculateEmissions.ts
@@ -9,8 +9,6 @@ import {
     DeviceName,
 } from "../../data/constants/DeviceEmissions";
 
-const BYTES_PER_GB = 1073741824;
-
 export const calculateEmissions = (
     bytes: number,
     selectedCountries: Map<CountryName, number>
@@ -22,13 +20,13 @@ export const calculateEmissions = (
     _selectedCountries.forEach((percentage, country) => {
         totalPercentage += percentage;
         carbon +=
-            (bytes / BYTES_PER_GB) *
+            (bytes / (10 ^ 9)) *
             COUNTRY_CO2_EMISSIONS_GRAMS_PER_KWH[country] *
             percentage;
     });
 
     carbon +=
-        (bytes / BYTES_PER_GB) *
+        (bytes / (10 ^ 9)) *
         WORLD_AVERAGE_CO2_EMISSIONS_GRAMS_PER_KWH *
         (1 - totalPercentage);
 
@@ -50,7 +48,7 @@ export const calculateHardwareEmissions = (
 };
 
 export const calculateEnergyConsumptionkWh = (bytesTransferred: number) => {
-    const dataTransferredGb = (bytesTransferred / 10) ^ 9;
+    const dataTransferredGb = bytesTransferred / (10 ^ 9);
     const energykWhPerGb = 0.81;
     return dataTransferredGb * energykWhPerGb;
 };
